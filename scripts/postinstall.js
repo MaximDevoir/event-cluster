d28b3a42953388b3d2e87e1ce0b8d0a5fa4c81a3
Maxim Devoir <maximdevoir@hotmail.com>
2016-06-08 21:55:07 -0600

    Fully implemented postinstall

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -2,16 +2,20 @@
 var fs = require('fs');
 var exec = require('child_process').exec;
 var path = require('path');
+var jsdocFile = path.resolve(__dirname, '..', 'node_modules', '.bin', 'jsdoc');
+console.log(jsdocFile);
 
-fs.access(path.join(__dirname, '..', 'node_modules', '.bin', 'jsdoc'), fs.F_OK, function(err) {
-  if (!err) {
-    console.log("Can\'t generate documentation. Dev dependencies not installed.")
+fs.access(jsdocFile, fs.F_OK, function(err) {
+  if (err) {
+    console.log("Can\'t generate documentation. Dev dependencies not installed.");
   } else {
     exec("npm run build:docs", {
       cwd: path.resolve(__dirname, '..')
-    }, function (error){
-      console.log("Could\'t generate documentation.");
-      throw error;
+    }, function (error) {
+      if (error) {
+        console.log("Could\'t generate documentation.");
+        console.log('Postinstall error', error);
+      }
     });
   }
 });
